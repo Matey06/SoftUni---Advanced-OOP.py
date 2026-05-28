8e18a9b5c46944e7b16d6d9456e87238088ccfbe
Matey06 <matey.mateev2006@gmail.com>
2026-05-28 20:41:56 +0300

    problems solved

---
+++ 05_functions_advanced/01_lab/03_cheese_showcase.py
@@ -0,0 +1,25 @@
+def sorting_cheeses(**kwargs):
+    sorted_cheeses = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+    result = ""
+    for cheese, quantities in sorted_cheeses:
+        result += f"{cheese}\n"
+        for quantity in sorted(quantities, reverse=True):
+            result += f"{quantity}\n"
+    return result
+
+# Test case
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
+# Test case
+print(
+    sorting_cheeses(
+        Parmigiano=[165, 215],
+        Feta=[150, 515],
+        Brie=[150, 125]
+    )
+)
---
+++ 05_functions_advanced/01_lab/06_operate.py
@@ -0,0 +1,31 @@
+from functools import reduce
+
+def sum_nums(*args):
+    return reduce(lambda x, y: x + y, args)
+
+
+def sub_nums(*args):
+    return reduce(lambda x, y: x - y, args)
+
+
+def mul_nums(*args):
+    return reduce(lambda x, y: x * y, args)
+
+
+def div_nums(*args):
+    return reduce(lambda x, y: x / y, args)
+
+
+def operate(operator, *args):
+    operation = mapper[operator]
+    return operation(*args)
+
+mapper = {
+    "+": sum_nums,
+    "-": sub_nums,
+    "*": mul_nums,
+    "/": div_nums,
+}
+
+print(operate("+", 1, 2, 3))
+print(operate("*", 3, 4))
