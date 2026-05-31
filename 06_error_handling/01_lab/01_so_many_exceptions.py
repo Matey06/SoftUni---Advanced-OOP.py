9f7afd62d4ee5d93585468f19973d69df58c3df9
Matey06 <matey.mateev2006@gmail.com>
2026-05-31 19:19:50 +0300

    File Added

--- 06_error_handling/01_lab/01_so_many_exceptions.py
+++ 06_error_handling/01_lab/01_so_many_exceptions.py
@@ -1,3 +1,18 @@
+# #Raw Code Before Changes
+
+# numbers_list = int(input()).split(", ")
+# result = 1
+#
+# for i in range(numbers_list):
+#     number = numbers_list[i+1]
+#     if number <= 5
+#         result *= number
+#     elif 5 < number <= 10:
+#         result /= number
+# print(total)
+
+
+#Corrected Version
 numbers_list = [int(x) for x in (input()).split(", ")]
 result = 1
 
